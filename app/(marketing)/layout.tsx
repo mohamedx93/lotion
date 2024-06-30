b06ce44494c19dde39dc72af06dd2ccd5887574c
mohamedx93 <mabnf93@gmail.com>
2024-06-30 21:44:08 +0100

    some fixes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,7 +4,7 @@ const MarketingLayout: React.FunctionComponent<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
-    <div className="h-full dark:bg-neutral-900">
+    <div className="h-full dark:bg-[#1f1f1f]">
       <Navbar />
       <main className="h-full pt-40">{children}</main>
     </div>
